13ab8ea47637a28da7cd249c5f5e31e89492580f
ice <ice.feng@unokiwi.com>
2020-03-11 20:33:14 -0700

    yea

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>app</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<!-- jQuery (Bootstrap's JavaScript plugin need to import jQuery) -->
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<!-- include all complied plugin -->
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+
+
+</body>
+
+</html>
